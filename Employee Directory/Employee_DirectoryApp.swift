6c56d813b8cb2481720f1b9692b653a0e928ffcb
Libby Smetak <smetak.libby@heb.com>
2022-12-01 16:23:16 -0500

    Initial Commit

---
+++ Employee Directory/Employee_DirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employee_DirectoryApp.swift
+//  Employee Directory
+//
+//  Created by Smetak,Libby on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Employee_DirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
