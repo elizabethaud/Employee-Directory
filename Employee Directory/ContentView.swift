42e4e4d0457a53705498929432020113a497bd47
elizabethaud <elizabethaud@outlook.com>
2022-12-01 19:25:19 -0500

    reorganization

--- Employee Directory/ContentView.swift
+++ Employee Directory/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = DirectoryViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, Earth!")
+            ForEach(viewModel.employees) { employee in
+                Text(employee.name)
+            }
         }
         .padding()
     }
